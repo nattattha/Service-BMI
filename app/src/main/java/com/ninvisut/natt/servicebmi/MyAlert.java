bb39eaf638d198eaddbdc193336df605a44ea096
natt <atthawadee02@gmail.com>
2016-09-07 11:30:47 +0700

    Check Space

---
+++ app/src/main/java/com/ninvisut/natt/servicebmi/MyAlert.java
@@ -0,0 +1,30 @@
+package com.ninvisut.natt.servicebmi;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by NATT-PC on 9/7/2016.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         int intIcon,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(intIcon);
+        builder.setTitle(strTitle);
+        builder.setTitle(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+}   //Main
--- app/src/main/java/com/ninvisut/natt/servicebmi/SignUpActivity.java
+++ app/src/main/java/com/ninvisut/natt/servicebmi/SignUpActivity.java
@@ -2,12 +2,50 @@ package com.ninvisut.natt.servicebmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText,
+            passwordEditText, rePasswordEditText;
+    private String nameString, userString,
+            passwordString, rePasswordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
+
+        //bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        rePasswordEditText = (EditText) findViewById(R.id.editText4);
+
+    }   //onCreate
+
+    public void clickSignUpSign(View view) {
+
+        //Get Value Form Edit Text
+
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+        rePasswordString = rePasswordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") ||
+                userString.equals("") ||
+                passwordString.equals("") ||
+                rePasswordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, R.drawable.doremon48,
+                    "มีช่องว่าง", "กรุณากรอกให้ครบ");
+        }
+
+    }   //clickSignUp
 }
